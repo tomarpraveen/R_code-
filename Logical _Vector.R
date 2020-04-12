691cedc1c9cfe5112e2b2a60c7d3548f0c8573eb
Praveen tomar <praveengwalior10@gmail.com>
2020-04-13 00:03:04 +0530

    Swirl_Logical Vector Pracatice

---
+++ Logical _Vector.R
@@ -0,0 +1,25 @@
+#
+library(swirl)
+swirl()
+praveen
+2
+num_vect<-c(0.5,55,-10,6)
+
+tf<-num_vect<1
+1
+tf
+
+num_vect>=6
+2
+1
+2
+my_char<-c("My","name","is")
+my_char
+
+paste(my_char, collapse = " ")
+
+my_name<- c(my_char,"Praveen")
+info()
+skip()
+nxt
+
